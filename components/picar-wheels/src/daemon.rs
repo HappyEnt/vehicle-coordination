d288dd8f36726da10ba9d692abb946ae8e232098
Louis Meyer <stu208438@mail.uni-kiel.de>
2022-07-07 15:40:41 +0200

    Slowly switch to desired speed

--- components/picar-wheels/src/daemon.rs
+++ components/picar-wheels/src/daemon.rs
@@ -1,3 +1,7 @@
+use std::thread;
+use std::time::Duration;
+
+use async_channel::{Receiver, Sender};
 use rppal::pwm::{Channel, Polarity, Pwm};
 use tonic::{transport::Server, Request, Response, Status};
 
@@ -12,8 +16,7 @@ const HERTZ: f64 = 50.0;
 
 #[derive(Debug)]
 pub struct PicarService {
-    pwm_left: Pwm,
-    pwm_right: Pwm,
+    channel: Sender<(f64, f64)>,
 }
 
 #[tonic::async_trait]
@@ -45,11 +48,7 @@ impl Picar for PicarService {
         let duty_ratio_r =
             min_duty_ratio + (max_duty_ratio - min_duty_ratio) * (0.5 * (right + 1.0));
 
-        // try to set pwm speeds
-        if let Err(_) = self.pwm_left.set_duty_cycle(duty_ratio_l) {
-            eprintln!("Could not set PWM duty cycle");
-            Err(Status::internal("Could not set PWM duty cycle"))
-        } else if let Err(_) = self.pwm_right.set_duty_cycle(duty_ratio_r) {
+        if let Err(_) = self.channel.send((duty_ratio_l, duty_ratio_r)).await {
             eprintln!("Could not set PWM duty cycle");
             Err(Status::internal("Could not set PWM duty cycle"))
         } else {
@@ -62,13 +61,57 @@ impl Picar for PicarService {
     }
 }
 
+impl PicarService {
+    fn start(&self, receiver: Receiver<(f64, f64)>) {
+        thread::spawn(move || {
+            let pwm_left =
+                Pwm::with_frequency(Channel::Pwm0, HERTZ, 0.075, Polarity::Normal, true).unwrap();
+            let pwm_right =
+                Pwm::with_frequency(Channel::Pwm1, HERTZ, 0.075, Polarity::Normal, true).unwrap();
+
+            let mut current_l = 0.075;
+            let mut current_r = 0.075;
+
+            let mut desired_l = current_l;
+            let mut desired_r = current_r;
+
+            loop {
+                match receiver.try_recv() {
+                    Err(_) => (),
+                    Ok((left, right)) => {
+                        desired_l = left;
+                        desired_r = right;
+                    }
+                };
+
+                let left_diff = desired_l - current_l;
+                let left_change = 0.05_f64.min(left_diff.abs()) * left_diff.signum();
+                current_l += left_change;
+
+                let right_diff = desired_r - current_r;
+                let right_change = 0.05_f64.min(right_diff.abs()) * right_diff.signum();
+                current_r += right_change;
+
+                if let Err(_) = pwm_left.set_duty_cycle(current_l) {
+                    eprintln!("Could not set PWM duty cycle");
+                } else if let Err(_) = pwm_right.set_duty_cycle(current_r) {
+                    eprintln!("Could not set PWM duty cycle");
+                }
+                thread::sleep(Duration::from_millis(100));
+            }
+        });
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let picar_service = PicarService {
-        pwm_left: Pwm::with_frequency(Channel::Pwm0, HERTZ, 0.075, Polarity::Normal, true)?,
-        pwm_right: Pwm::with_frequency(Channel::Pwm1, HERTZ, 0.075, Polarity::Normal, true)?,
-    };
+
+    let (sender, receiver) = async_channel::unbounded::<(f64, f64)>();
+
+    let picar_service = PicarService { channel: sender };
+
+    picar_service.start(receiver);
 
     Server::builder()
         .add_service(PicarServer::new(picar_service))
