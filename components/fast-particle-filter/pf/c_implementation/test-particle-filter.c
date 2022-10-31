8ee5b0df053630850564d2b448d150c101c389c5
Christian Richter <christian@MacBook-Air.local>
2022-10-31 23:56:39 +0100

    Removed simulation and fast-particle-filter folder
    superceded by ros component integrating everything together

--- components/fast-particle-filter/pf/c_implementation/test-particle-filter.c
+++
@@ -1,9 +0,0 @@
-#include "particle-belief-propagation.h"
-
-int main(int argc, char *argv[]) {
-  // TODO implement with new API
-  struct particle_filter_instance *instance;
-  create_particle_filter_instance(&instance);
-  destroy_particle_filter_instance(instance);  
-  return 0;
-}
