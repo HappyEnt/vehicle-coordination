b66a5ddb3647135ee22f6fd479badad8ab2c9ffd
Christian Richter <christian@MacBook-Air.local>
2022-11-03 18:30:56 +0100

    Set Wheel velocity depending on angular and linear velocity

--- components/ros/src/orcar_wheels/orcar_wheels/orcar_servo_node.py
+++ components/ros/src/orcar_wheels/orcar_wheels/orcar_servo_node.py
@@ -6,9 +6,13 @@ from rpi_hardware_pwm import HardwarePWM
 
 HALF_DISTANCE_BETWEEN_WHEELS = 0.047
 WHEEL_RADIUS = 0.0299
+WHEEL_DIAMETER = 2 * np.pi * WHEEL_RADIUS
 
 BASE_FREQUENCY = 50.0
 BASE_PERIOD = 1000.0 / BASE_FREQUENCY
+
+MAX_RPM = 110 # from datasheet, could deviate in practice depending on the load and supply voltage
+MAX_VELOCITY = WHEEL_DIAMETER * MAX_RPM / 60.0
 
 FULL_BACKWARD_DUTY_CYCLE   = 1.0 / BASE_PERIOD * 100
 STOP_DUTY_CYCLE            = 1.5 / BASE_PERIOD * 100
@@ -25,14 +29,27 @@ class OrcarServoNode(Node):
         # create cmd_vel topic for receiving Twist messages
         cmd_vel_subscription = self.create_subscription(Twist, '~/cmd_vel', self.__cmd_vel_callback, 1)
 
-    def __cmd_vel_callback(self, msg):
-        # extract from Twist speeds for both motors
-        left_speed = msg.linear.x - msg.angular.z * HALF_DISTANCE_BETWEEN_WHEELS / WHEEL_RADIUS
-        right_speed = msg.linear.x + msg.angular.z * HALF_DISTANCE_BETWEEN_WHEELS / WHEEL_RADIUS
+    def setVelocity(self, left_velocity, right_velocity):
+        # Define maximum velocity
+        fraction_left = left_velocity / MAX_VELOCITY
+        fraction_right = right_velocity / MAX_VELOCITY
+
+        # clamp values
+        fraction_left = max(min(fraction_left, 1.0), -1.0)
+        fraction_right = max(min(fraction_right, 1.0), -1.0)
 
         # set motor speed
-        self.pwm_left.change_duty_cycle(STOP_DUTY_CYCLE  + (FULL_FORWARD_DUTY_CYCLE - STOP_DUTY_CYCLE) * msg.linear.x)
-        self.pwm_right.change_duty_cycle(STOP_DUTY_CYCLE  + (FULL_FORWARD_DUTY_CYCLE - STOP_DUTY_CYCLE) * msg.linear.x)
+        self.pwm_left.change_duty_cycle(STOP_DUTY_CYCLE  + (FULL_FORWARD_DUTY_CYCLE - STOP_DUTY_CYCLE) * fraction_left)
+        self.pwm_right.change_duty_cycle(STOP_DUTY_CYCLE  + (FULL_FORWARD_DUTY_CYCLE - STOP_DUTY_CYCLE) * fraction_right)
+
+    def __cmd_vel_callback(self, msg):
+        # extract from Twist speeds for both motors
+        left_vel  = (msg.linear.x - msg.angular.z * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
+        right_vel = (msg.linear.x + msg.angular.z * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
+
+        self.setVelocity(left_vel, right_vel)
+
+
 
 def main(args=None):
     rclpy.init(args=None)
